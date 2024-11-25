bd1a33408f8720a0dc695fcbae58024801c7e667
sirapatkan <summonspaw@gmail.com>
2024-11-25 14:45:06 +0700

    second commit

---
+++ share.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+ 
+void main(){
+  print("ยอดรวม : ");
+  double? TotalBillAmount = double.parse(stdin.readLineSync()!);;
+  print("จำนวนคน : ");
+  int NumOfPeople = int.parse(stdin.readLineSync()!);
+  double AmountToPaid = TotalBillAmount/NumOfPeople;
+  print("จำนวนที่ต้องจ่าย(ต่อคน) : $AmountToPaid");
+ 
+ 
+ 
+}
+ 
